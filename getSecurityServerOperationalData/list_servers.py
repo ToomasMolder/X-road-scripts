d80cd831879f1dcf681a1ef0a46fd0591c0efcc5
Toomas Mölder <toomas.molder@ria.ee>
2017-05-29 14:36:10 +0300

    Create list_servers.py

---
+++ getSecurityServerOperationalData/list_servers.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+# NB! Global configuration signature is not checked. Use this program at your own risk.
+
+import sys
+import re
+import requests
+import xml.etree.ElementTree as ET
+
+# Timeout for http requests
+TIMEOUT=5.0
+
+if len(sys.argv) != 2 or sys.argv[1] in ("-h", "-help", "--help"):
+    print "Usage: ${0} <central_server_name>\nThe Name/IP of Central server can be found in configuration anchor."
+    exit(0)
+
+# Name/IP of Central server
+SERVER=sys.argv[1]
+
+# Downloading shared-params.xml
+try:
+    globalConf = requests.get("http://"+SERVER+"/internalconf", timeout=TIMEOUT)
+    globalConf.raise_for_status()
+# NB! re.search global configuration regex might be changed according version naming or other future naming conventions
+    s = re.search("Content-location: (/V\d+/\d+/shared-params.xml)", globalConf.content)
+    sharedParams = requests.get("http://"+SERVER+s.group(1), timeout=TIMEOUT)
+    sharedParams.raise_for_status()
+except requests.exceptions.RequestException:
+    exit(0)
+
+try:
+    root = ET.fromstring(sharedParams.content)
+    instance = root.find("./instanceIdentifier").text
+    for server in root.findall("./securityServer"):
+        ownerId = server.find("./owner").text
+        owner = root.find("./member[@id='"+ownerId+"']")
+        memberClass = owner.find("./memberClass/code").text
+        memberCode = owner.find("./memberCode").text
+        serverCode = server.find("./serverCode").text
+        address = server.find("./address").text
+        print instance + "/" + memberClass + "/" + memberCode + "/" + serverCode + "/" + address
+except Exception:
+    exit(0)
+
