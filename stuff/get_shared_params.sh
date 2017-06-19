a527feaf2fbc7f05ead3eb1c568fd80e588352c0
Toomas Mölder <toomas.molder@ria.ee>
2017-06-19 15:47:38 +0300

    Update get_shared_params.sh

--- stuff/get_shared_params.sh
+++ stuff/get_shared_params.sh
@@ -3,7 +3,7 @@
 # NB!!! Global configuration signature is not verified! Do not rely on the validity of this data!
 
 if [ -z $1 ] || [ $1 = "-h" ] || [ $1 = "--help" ]; then
-    echo -e "Usage: get_shared_params.sh <central_server_URL>\nThe URL of Central server can be found in configuration anchor"
+    echo -e "Usage: $0 <central_server_URL>\nThe URL of Central server can be found in configuration anchor"
     exit 1;
 fi
 
