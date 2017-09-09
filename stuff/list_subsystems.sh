38c6e5b379b6a13dccc9972d145caf60dccef175
Toomas Mölder <toomas.molder@ria.ee>
2017-09-09 10:25:51 +0300

    Update list_subsystems.sh

--- stuff/list_subsystems.sh
+++ stuff/list_subsystems.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ $1 = "-h" ] || [ $1 = "--help" ]; then
-    echo -e "Usage: $0 <security_server_URL>
-Example: $0 server.ci.kit"
+if [ -z ${1} ] || [ ${1} = "-h" ] || [ ${1} = "--help" ]; then
+    echo -e "Usage: ${0} <security_server_IP/name>
+Example: ${0} server.ci.kit"
     exit 1;
 fi
 
-SERVER=$1
+SERVER=${1}
+
+: '
+Quick installation of xmlstarlet:
+Step 1: Update system:
+        sudo apt-get update
+Step 2: Install: xmlstarlet
+Ater updaing the OS run following command to install the packae:
+        sudo apt-get install xmlstarlet
+'
 
 curl -s ${SERVER}/listClients | xmlstarlet sel -t -m '//ns2:member/ns2:id[@ns1:objectType="SUBSYSTEM"]' \
     -v "concat(ns1:xRoadInstance, '/', ns1:memberClass, '/', ns1:memberCode, '/', ns1:subsystemCode)" -n
