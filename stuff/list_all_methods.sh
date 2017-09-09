a0dfd6f082d04642c4d0bb6cbe8557ac09062fec
Toomas Mölder <toomas.molder@ria.ee>
2017-09-09 10:28:08 +0300

    Update list_all_methods.sh

--- stuff/list_all_methods.sh
+++ stuff/list_all_methods.sh
@@ -3,7 +3,7 @@
 if [ -z ${1} ] || [ -z ${2} ] || [ ${1} = "-h" ] || [ ${1} = "--help" ]; then
     echo -e "Usage: ${0} <security_server_URL> <consumer_member_identifier>
 Example: ${0} server.ci.kit VS/GOV/20000001"
-    exit 1;
+    exit 1
 fi
 
 SERVER=${1}
