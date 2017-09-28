f04fb5c3d05b6085e642539edb260e9f5bb991b3
Toomas Mölder <toomas.molder@ria.ee>
2017-09-28 16:52:11 +0300

    Create list_subsystems_with_server.py

---
+++ riha/list_subsystems_with_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# NB! Global configuration signature is not checked. Use this program at your own risk.
+
+import sys
+import re
+import requests
+import xml.etree.ElementTree as ET
+
+# Timeout for http requests
+TIMEOUT=5.0
+
+if len(sys.argv) != 2 or sys.argv[1] in ("-h", "-help", "--help"):
+    print "Usage: list_subsystem_server.py <central_server_name>\nThe Name/IP of Central server can be found in configuration anchor."
+    exit(0)
+
+# Name/IP of Central server
+SERVER=sys.argv[1]
+
+# Downloading shared-params.xml
+try:
+    globalConf = requests.get("http://"+SERVER+"/internalconf", timeout=TIMEOUT)
+    globalConf.raise_for_status()
+    s = re.search("Content-location: (/.+/shared-params.xml)", globalConf.content)
+    sharedParams = requests.get("http://"+SERVER+s.group(1), timeout=TIMEOUT)
+    sharedParams.raise_for_status()
+except requests.exceptions.RequestException:
+    exit(0)
+
+try:
+    root = ET.fromstring(sharedParams.content)
+    instance = root.find("./instanceIdentifier").text
+    for member in root.findall("./member"):
+        memberId = member.attrib["id"]
+        memberClass = member.find("./memberClass/code").text
+        memberCode = member.find("./memberCode").text
+        for subsystem in member.findall("./subsystem"):
+            subsystemId = subsystem.attrib["id"]
+            subsystemCode = subsystem.find("./subsystemCode").text
+            foundServer = False
+            if root.findall("./securityServer[client='{}']".format(subsystemId)):
+                print "{}/{}/{}/{}".format(instance, memberClass, memberCode, subsystemCode.encode('utf8'))
+except Exception as e:
+    print e
+    exit(0)
