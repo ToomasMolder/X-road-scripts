6b2d230d45237d104865ac6b46341cbd501c5a86
Toomas Mölder <toomas.molder@ria.ee>
2017-09-28 16:55:47 +0300

    Update list_subsystems_with_server.py

--- riha/list_subsystems_with_server.py
+++ riha/list_subsystems_with_server.py
@@ -11,7 +11,7 @@ import xml.etree.ElementTree as ET
 TIMEOUT=5.0
 
 if len(sys.argv) != 2 or sys.argv[1] in ("-h", "-help", "--help"):
-    print "Usage: list_subsystem_server.py <central_server_name>\nThe Name/IP of Central server can be found in configuration anchor."
+    print("Usage: list_subsystem_server.py <central_server_name>\nThe Name/IP of Central server can be found in configuration anchor.")
     exit(0)
 
 # Name/IP of Central server
@@ -19,12 +19,13 @@ SERVER=sys.argv[1]
 
 # Downloading shared-params.xml
 try:
-    globalConf = requests.get("http://"+SERVER+"/internalconf", timeout=TIMEOUT)
+    globalConf = requests.get("http://{}/internalconf".format(SERVER), timeout=TIMEOUT)
     globalConf.raise_for_status()
-    s = re.search("Content-location: (/.+/shared-params.xml)", globalConf.content)
-    sharedParams = requests.get("http://"+SERVER+s.group(1), timeout=TIMEOUT)
+    s = re.search("Content-location: (/.+/shared-params.xml)", globalConf.content.decode('utf-8'))
+    sharedParams = requests.get("http://{}{}".format(SERVER, s.group(1)), timeout=TIMEOUT)
     sharedParams.raise_for_status()
-except requests.exceptions.RequestException:
+except requests.exceptions.RequestException as e:
+    print(e)
     exit(0)
 
 try:
@@ -39,7 +40,10 @@ try:
             subsystemCode = subsystem.find("./subsystemCode").text
             foundServer = False
             if root.findall("./securityServer[client='{}']".format(subsystemId)):
-                print "{}/{}/{}/{}".format(instance, memberClass, memberCode, subsystemCode.encode('utf8'))
+                if sys.version_info[0] < 3:
+                    print("{}/{}/{}/{}".format(instance, memberClass, memberCode, subsystemCode.encode('utf-8')))
+                else:
+                    print("{}/{}/{}/{}".format(instance, memberClass, memberCode, subsystemCode))
 except Exception as e:
-    print e
+    print(e)
     exit(0)
